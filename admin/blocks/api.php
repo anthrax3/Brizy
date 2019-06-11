30c37eae4782f02124e26d5dd002928520d1d9c7
Alex Zaharia <alecszaharia@gmail.com>
2019-06-11 16:25:52 +0300

    Modified block api

--- admin/blocks/api.php
+++ admin/blocks/api.php
@@ -171,7 +171,7 @@ class Brizy_Admin_Blocks_Api extends Brizy_Admin_AbstractApi {
 			do_action( 'brizy_global_block_created', $block );
 			do_action( 'brizy_global_data_updated' );
 
-			$this->success( $block->createResponse() );
+			$this->success( Brizy_Editor_Block::postData($block->createResponse()) );
 
 		} catch ( Exception $exception ) {
 			$this->error( 400, $exception->getMessage() );
@@ -210,7 +210,7 @@ class Brizy_Admin_Blocks_Api extends Brizy_Admin_AbstractApi {
 			do_action( 'brizy_saved_block_created', $block );
 			do_action( 'brizy_global_data_updated' );
 
-			$this->success( $block->createResponse() );
+			$this->success( Brizy_Editor_Block::postData($block->createResponse()) );
 
 		} catch ( Exception $exception ) {
 			$this->error( 400, $exception->getMessage() );
@@ -487,7 +487,6 @@ class Brizy_Admin_Blocks_Api extends Brizy_Admin_AbstractApi {
 		throw new Exception( 'Unable to create block' );
 	}
 
-
 	/**
 	 * @param $postUid
 	 * @param $postType
@@ -503,6 +502,4 @@ class Brizy_Admin_Blocks_Api extends Brizy_Admin_AbstractApi {
 			return wp_delete_post( $postId );
 		}
 	}
-
-
 }
